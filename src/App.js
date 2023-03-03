09b937c853f37f1461512a9a17795b61c731cd70
amirthenew <ah.abshari@hotmail.com>
2023-03-04 01:28:16 +0330

    product details page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import './App.css';
 import {Route,Routes,Navigate} from'react-router-dom'
 import Store from './Store';
+import ProductDetails from './shared/ProductDetails';
 //context
 import ProductContextProvider from './context/ProductContextProvider';
 
@@ -12,6 +13,7 @@ function App() {
   return (
    <ProductContextProvider>
    <Routes>
+   <Route path='/products/:id' element={<ProductDetails/>} />
    <Route path='/products' element={<Store/>} />
    <Route path='/*' element={<Navigate to ='/products'/>}/>
    </Routes>
---
+++ src/shared/ProductDetails.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ProductDetails = () => {
+    return ( 
+        <h2>test</h2>
+     );
+}
+ 
+export default ProductDetails;
