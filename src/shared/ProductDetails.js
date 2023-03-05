b41d454c6b4c3a614ff8e0e2f80157f7386c72bf
amirthenew <ah.abshari@hotmail.com>
2023-03-05 23:49:53 +0330

    have problem

--- src/shared/ProductDetails.js
+++ src/shared/ProductDetails.js
@@ -1,17 +1,32 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { ProductsContext } from '../context/ProductContextProvider';
+import Product from './Product';
 
 
 const ProductDetails = (props) => {
 
     
-const data = useContext(ProductsContext)
+
     console.log(data);
+    const id = props.match.params.id
+    const data = useContext(ProductsContext)
+    const product = data[id-1]
+    const {image,title,description,price,category} = product
 
 
     return ( 
-<h1>details</h1>
+        <div>
+        <h1>details</h1>
+        <h2>{title}</h2>
+        <p>{description}</p>
+        <p><span>catgory : </span>{category}</p>
+        <div>
+        <span>{price}$</span>
+        <Link to='/products'>back to shop</Link>
+        </div>
+        
+        </div>
      );
 }
  
