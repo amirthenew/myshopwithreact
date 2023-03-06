a33b113671b84ee102a702d807e21a40158415fd
amirthenew <ah.abshari@hotmail.com>
2023-03-06 23:06:38 +0330

    product details page is fixed(use params)

--- src/shared/ProductDetails.js
+++ src/shared/ProductDetails.js
@@ -2,18 +2,19 @@ import React, { useContext } from 'react';
 import { Link ,useParams} from 'react-router-dom';
 import { ProductsContext } from '../context/ProductContextProvider';
 
-const ProductDetails = (props) => {
-console.log(props);
-    // const id = props.match.params.id;
+const ProductDetails = () => {
+const params = useParams()
+    const id = params.id
     const data = useContext(ProductsContext)
-    const product = data[1]
-    const {image, title, description, price, category} = product;
-    console.log(title);
+    const product = data[id-1]
+    const {image,title, description, price, category} = product;
+    console.log(product);
 
     return ( 
 
 
         <div>
+
         <h1>details</h1>
         <img src={image}/>
         <h2>{title}</h2>
