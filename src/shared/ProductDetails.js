16d6adc0bc78b02ac9b0c04c25b4f27822d4e67c
amirthenew <ah.abshari@hotmail.com>
2023-03-06 22:54:48 +0330

    import useparams

--- src/shared/ProductDetails.js
+++ src/shared/ProductDetails.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link ,useParams} from 'react-router-dom';
 import { ProductsContext } from '../context/ProductContextProvider';
 
 const ProductDetails = (props) => {
