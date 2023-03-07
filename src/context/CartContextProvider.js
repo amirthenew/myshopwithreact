606d8bf21fd2fe97a2bc3d96a6eac134b450172e
amirthenew <ah.abshari@hotmail.com>
2023-03-07 23:58:55 +0330

    cart reducer add item

--- src/context/CartContextProvider.js
+++ src/context/CartContextProvider.js
@@ -1,14 +1,26 @@
 
 import React,{useReducer} from 'react';
 
-const initialState ={
+const initialState = {
     selectedItems : [],
     itemsCounter : 0,
     total : 0,
     checkout : false
 }
 
+const cartReducer = (state,action) =>{
+    switch(action.type){
+        case 'ADD_ITEM':
+            if (!state.selectedItems.find(item=>item.id === action.payload.id)) {
+                state.selectedItems.push(
+                    {
+                        ...action.payload,
+                    }
+                )
+            }
 
+    }
+}
 
 const CartContextProvider = () => {
 
