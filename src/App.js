b20a6052cd92e17fdf3e2d0fa412e371cf32a2af
amirthenew <ah.abshari@hotmail.com>
2023-03-10 23:55:21 +0330

    CartContextProvider

--- src/App.js
+++ src/App.js
@@ -8,20 +8,22 @@ import Header from './Header';
 import ProductDetails from './shared/ProductDetails';
 //context
 import ProductContextProvider from './context/ProductContextProvider';
-
+import CartContextProvider from './context/CartContextProvider';
 
 function App() {
 
 
   return (
    <ProductContextProvider>
+   <CartContextProvider>
    <Header/>
    <Routes>
    <Route path='/products/:id' element={<ProductDetails/>} />
    <Route path='/products' element={<Store/>} />
    <Route path='/*' element={<Navigate to ='/products'/>}/>
    </Routes>
-   
+   </CartContextProvider>
+
    </ProductContextProvider>
   );
 }
