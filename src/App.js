2512f0ffcd9b7ff9faff68c2c0f1eb057cfd5db1
amirthenew <ah.abshari@hotmail.com>
2023-03-10 23:39:03 +0330

    increase

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 import {Route,Routes,Navigate} from'react-router-dom'
 //components
 import Store from './Store';
+import Header from './Header';
 import ProductDetails from './shared/ProductDetails';
 //context
 import ProductContextProvider from './context/ProductContextProvider';
-import Header from './Header';
 
 
 function App() {
