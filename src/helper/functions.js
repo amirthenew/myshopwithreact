9efb046c7e268ee4c314a21b8d9248a874d8b67d
amirthenew <ah.abshari@hotmail.com>
2023-03-12 00:11:43 +0330

    quantity count function

--- src/helper/functions.js
+++ src/helper/functions.js
@@ -11,4 +11,14 @@ const isInCart = (state,id)=>{
     const result = !!state.selectedItems.find((item)=> item.id ===id)
     return result
 }
-export {shorter,isInCart}
+
+const quantityCount = (state,id)=>{
+    const index = state.selectedItems.findIndex(item=>item.id === id)
+    if (index === -1){
+        return false
+    }
+    else {
+        return state.selectedItems[index].quantity
+    }
+}
+export {shorter,isInCart,quantityCount}
