a1007f943181b03b96c206a99a2f8f52ed01e175
amirthenew <ah.abshari@hotmail.com>
2023-03-24 00:36:52 +0430

    import cart

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {Route,Routes,Navigate} from'react-router-dom'
 import Store from './Store';
 import Header from './Header';
 import ProductDetails from './shared/ProductDetails';
+import Cart from './Cart';
 //context
 import ProductContextProvider from './context/ProductContextProvider';
 import CartContextProvider from './context/CartContextProvider';
@@ -20,6 +21,7 @@ function App() {
    <Routes>
    <Route path='/products/:id' element={<ProductDetails/>} />
    <Route path='/products' element={<Store/>} />
+   <Route path='/cart' element={<Cart/>} />
    <Route path='/*' element={<Navigate to ='/products'/>}/>
    </Routes>
    </CartContextProvider>
--- src/Cart.js
+++ src/Cart.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React,{useContext} from 'react';
 import styles from './Cart.module.css'
 
+//context
+import { CartContext } from './context/CartContextProvider';
 
 
 const Cart = () => {
+  
+const {state,dispatch} = useContext(CartContext)
     return (
 
         <div className={styles.container}>
         <span>my Cart</span>
         <span>price</span>
         <button>Add to cart</button>
+
         </div>
       );
 }
