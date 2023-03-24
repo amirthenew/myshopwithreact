d4bfeeb04378a1539145fb38f5a78e28841f7aca
amirthenew <ah.abshari@hotmail.com>
2023-03-25 00:55:20 +0430

    now link is working

--- src/Header.js
+++ src/Header.js
@@ -2,16 +2,19 @@ import React,{useContext} from 'react';
 import styles from './Header.module.css'
 import cart from '../src/img/cart.png'
 import { CartContext } from './context/CartContextProvider';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
 
   const {state} = useContext(CartContext)
-console.log(state);
+
     return (
         <div className={styles.header}>
         <h1>header</h1>
         <div className={styles.cart}>
+        <Link to='/cart'>
         <img className={styles.cart} src={cart}/>
+        </Link>
         <span>{state.itemsCounter}</span>
         </div>
 
