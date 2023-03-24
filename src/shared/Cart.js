591d5c4870f80f73e3e304c18cf6de42ec9f42da
amirthenew <ah.abshari@hotmail.com>
2023-03-25 01:51:49 +0430

    FontAwesome added successfully

--- src/shared/Cart.js
+++ src/shared/Cart.js
@@ -7,21 +7,24 @@ import { CartContext } from '../context/CartContextProvider';
 import {shorter} from '../helper/functions'
 //styles
 import styles from './Cart.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faTrash} from '@fortawesome/free-solid-svg-icons'
+
 
 const Cart = (props) => {
 
     const {dispatch} = useContext(CartContext);
     const {image, title, price, quantity} = props.data;
-
+    const faTrashs = <FontAwesomeIcon icon={faTrash}/>
     return (
 
         <div className={styles.container}>
-
             <img src={image} alt="product" />
             <div>
                 <h3>{shorter(title)}</h3>
                 <p>{price} $</p>
                 <span>quantity : {quantity}</span>
+                
             </div>
             <div>
                
@@ -30,7 +33,7 @@ const Cart = (props) => {
                 {
                     quantity > 1 ?
                     <button onClick={() => dispatch({type: "DECREASE", payload: props.data})}>-</button> :
-                    <button onClick={() => dispatch({type: "REMOVE_ITEM", payload: props.data})}>remove</button>
+                    <button className={styles.trash} onClick={() => dispatch({type: "REMOVE_ITEM", payload: props.data})}>{faTrashs}</button>
                 }
                 <button onClick={() => dispatch({type: "INCREASE", payload: props.data})}>+</button>
             </div>
