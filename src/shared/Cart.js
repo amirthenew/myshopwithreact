62b08b1a3fbe090763a2edd89c83da7ac35a7fbf
amirthenew <ah.abshari@hotmail.com>
2023-03-25 02:02:02 +0430

    icon added

--- src/shared/Cart.js
+++ src/shared/Cart.js
@@ -9,6 +9,8 @@ import {shorter} from '../helper/functions'
 import styles from './Cart.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faTrash} from '@fortawesome/free-solid-svg-icons'
+import {faPlus} from '@fortawesome/free-solid-svg-icons'
+import {faMinus} from '@fortawesome/free-solid-svg-icons'
 
 
 const Cart = (props) => {
@@ -16,6 +18,8 @@ const Cart = (props) => {
     const {dispatch} = useContext(CartContext);
     const {image, title, price, quantity} = props.data;
     const faTrashs = <FontAwesomeIcon icon={faTrash}/>
+    const PlusIcon = <FontAwesomeIcon icon={faPlus}/>
+    const minusIcon = <FontAwesomeIcon icon={faMinus}/>
     return (
 
         <div className={styles.container}>
@@ -32,10 +36,10 @@ const Cart = (props) => {
             <div className={styles.buttons}>
                 {
                     quantity > 1 ?
-                    <button onClick={() => dispatch({type: "DECREASE", payload: props.data})}>-</button> :
-                    <button className={styles.trash} onClick={() => dispatch({type: "REMOVE_ITEM", payload: props.data})}>{faTrashs}</button>
+                    <button  className={`${styles.icon} ${styles.minus}`} onClick={() => dispatch({type: "DECREASE", payload: props.data})}>{minusIcon}</button> :
+                    <button className={`${styles.icon} ${styles.trash}`} onClick={() => dispatch({type: "REMOVE_ITEM", payload: props.data})}>{faTrashs}</button>
                 }
-                <button onClick={() => dispatch({type: "INCREASE", payload: props.data})}>+</button>
+                <button className={`${styles.icon} ${styles.plus}`}  onClick={() => dispatch({type: "INCREASE", payload: props.data})}>{PlusIcon}</button>
             </div>
         </div>
     );
