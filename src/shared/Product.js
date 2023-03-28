459da273bc1383dab39410257b9c39a45368608f
amirthenew <ah.abshari@hotmail.com>
2023-03-28 17:42:30 +0430

    add icon to cart

--- src/shared/Product.js
+++ src/shared/Product.js
@@ -5,14 +5,21 @@ import './Product.css'
 import {shorter,isInCart,quantityCount} from '../helper/functions'
 import {Link } from 'react-router-dom';
 
-import trashSvg from '../img/trash.svg'
+//styles
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faTrash} from '@fortawesome/free-solid-svg-icons'
+import {faPlus} from '@fortawesome/free-solid-svg-icons'
+import {faMinus} from '@fortawesome/free-solid-svg-icons'
+
 
 
 //context
 import { CartContext } from '../context/CartContextProvider';
 
 const Product = ({productData}) => {
-
+    const faTrashs = <FontAwesomeIcon icon={faTrash}/>
+    const PlusIcon = <FontAwesomeIcon icon={faPlus}/>
+    const minusIcon = <FontAwesomeIcon icon={faMinus}/>
     const{state,dispatch} = useContext(CartContext);
     return ( <div className='container'>
 
@@ -24,11 +31,11 @@ const Product = ({productData}) => {
         <Link to={`/products/${productData.id}`}>details</Link>
         <div>
 
-        {quantityCount(state,productData.id) > 1 &&  <button onClick={()=>dispatch({type:'DECREASE',payload : productData})}>-</button>}
-        {quantityCount(state,productData.id) === 1 && <button onClick={()=>dispatch({type:'REMOVE_ITEM' ,payload :productData})}><img alt='remove' style={{width:'15px'}} src={trashSvg}/></button> }
+        {quantityCount(state,productData.id) > 1 &&  <button onClick={()=>dispatch({type:'DECREASE',payload : productData})}>{minusIcon}</button>}
+        {quantityCount(state,productData.id) === 1 && <button onClick={()=>dispatch({type:'REMOVE_ITEM' ,payload :productData})}>{faTrashs}</button> }
         {
             isInCart(state,productData.id) ?
-            <button onClick={()=>dispatch({type:'INCREASE',payload: productData})}>+</button> :
+            <button onClick={()=>dispatch({type:'INCREASE',payload: productData})}>{PlusIcon}</button> :
             <button onClick={()=>dispatch({type:'ADD_ITEM',payload: productData})}>Add to cart</button>
         }
 
