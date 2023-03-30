10d012bb1a565b5847deadee7dc9959664bc23f8
amirthenew <ah.abshari@hotmail.com>
2023-03-31 00:01:50 +0430

    cart icon

--- src/shared/Product.js
+++ src/shared/Product.js
@@ -10,6 +10,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faTrash} from '@fortawesome/free-solid-svg-icons'
 import {faPlus} from '@fortawesome/free-solid-svg-icons'
 import {faMinus} from '@fortawesome/free-solid-svg-icons'
+import {faCartPlus} from '@fortawesome/free-solid-svg-icons'
+
 
 
 
@@ -20,6 +22,7 @@ const Product = ({productData}) => {
     const faTrashs = <FontAwesomeIcon icon={faTrash}/>
     const PlusIcon = <FontAwesomeIcon icon={faPlus}/>
     const minusIcon = <FontAwesomeIcon icon={faMinus}/>
+    const cartPlus = <FontAwesomeIcon icon={faCartPlus}/> 
     const{state,dispatch} = useContext(CartContext);
     return ( <div className='container'>
 
@@ -33,11 +36,11 @@ const Product = ({productData}) => {
 
         {quantityCount(state,productData.id) > 1 &&  <button className='icon' onClick={()=>dispatch({type:'DECREASE',payload : productData})}>{minusIcon}</button>}
         {quantityCount(state,productData.id) === 1 && <button className='icon' onClick={()=>dispatch({type:'REMOVE_ITEM' ,payload :productData})}>{faTrashs}</button> }
-        {quantityCount(state,productData.id) > 0 && <span>{quantityCount(state,productData.id)}</span> }
+        {quantityCount(state,productData.id) > 0 && <span className='count'>{quantityCount(state,productData.id)}</span> }
         {
             isInCart(state,productData.id) ?
             <button className='icon' onClick={()=>dispatch({type:'INCREASE',payload: productData})}>{PlusIcon}</button> :
-            <button onClick={()=>dispatch({type:'ADD_ITEM',payload: productData})}>Add to cart</button>
+            <button className='icon' onClick={()=>dispatch({type:'ADD_ITEM',payload: productData})}>{cartPlus}</button>
         }
 
         
