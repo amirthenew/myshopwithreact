e6eef68932324fd53828bad2bddfa5c27dfe361e
amirthenew <ah.abshari@hotmail.com>
2023-04-09 00:59:07 +0430

    footer now have style

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import styles from './Footer.module.css'
 
 const Footer = () => {
-    return ( <div>
+    return ( <div className={styles.footer}>
         <h2>all rights reserved by webidemy</h2>
         </div> );
 }
