6c9fa445e9dba3c85427f0bae6f058fef9852bbb
amirthenew <ah.abshari@hotmail.com>
2023-04-19 14:49:19 +0430

    remove map

--- src/Store.js
+++ src/Store.js
@@ -8,20 +8,22 @@ const Store = () => {
 
     const products = useContext(ProductsContext)
     console.log(products);
-    products.map((item)=>{
-    console.log(item.category);
+    // products.map((item)=>{
+    // console.log(item.category);
     
-    })
+    // })
 
     return (
         <div className={styles.container}>
         
            {
             products.map(product=>
+                
             <Product
             key={product.id}
             productData={product}
             />
+
     
  ) } 
         </div>
